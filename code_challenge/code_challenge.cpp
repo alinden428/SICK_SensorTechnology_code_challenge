c30a2670090ca1661af739bae11ea38b45785efe
Alinden <7709005@student.swin.edu.au>
2021-11-25 04:02:06 +1100

    Add project files.

---
+++ code_challenge/code_challenge.cpp
@@ -0,0 +1,10 @@
+﻿// Title: Toy robot code challenge for SICK sensor technologies
+// Author: Alex Linden
+// Created: 24/11/2021
+// Function implementation
+
+#include "code_challenge.h"
+#include "iostream"
+
+using namespace std;
+
---
+++ code_challenge/main.cpp
@@ -0,0 +1,17 @@
+// Title: Toy robot code challenge for SICK sensor technologies
+// Author: Alex Linden
+// Created: 24/11/2021
+// Main function
+
+#include "code_challenge.h"
+#include "iostream"
+
+using namespace std;
+ 
+
+
+int main()
+{
+
+
+}
