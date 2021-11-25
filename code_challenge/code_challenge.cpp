cacf3a312c97c416923747b4620ebe58932e5af0
Alinden <7709005@student.swin.edu.au>
2021-11-25 16:39:14 +1100

    implementation begin

--- code_challenge/code_challenge.cpp
+++ code_challenge/code_challenge.cpp
@@ -1,4 +1,7 @@
-﻿// Title: Toy robot code challenge for SICK sensor technologies
+﻿
+
+
+// Title: Toy robot code challenge for SICK sensor technologies
 // Author: Alex Linden
 // Created: 24/11/2021
 // Function implementation
@@ -8,3 +11,33 @@
 
 using namespace std;
 
+myRobot::myRobot(unsigned short int X, unsigned short int Y, string dir)
+{
+	x = X;
+	y = Y;
+	direction = dir;
+}
+
+myRobot::~myRobot() {}
+
+void myRobot::place(unsigned short int new_x, unsigned short int new_y, string new_direction, myRobot newRobot)
+{
+	newRobot.x = new_x;
+	newRobot.y = new_y;
+	newRobot.direction = new_direction;
+}
+
+void myRobot::move()
+{
+
+}
+
+void myRobot::dontFall()
+{
+
+}
+
+void myRobot::report()
+{
+
+}
