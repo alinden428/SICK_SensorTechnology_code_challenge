3ca7ff68a67bd53724d1d6074230c73ace916062
Alinden <7709005@student.swin.edu.au>
2021-11-25 16:02:41 +1100

    Initial header file for future implementation. Will be modified

--- code_challenge/code_challenge.h
+++ code_challenge/code_challenge.h
@@ -7,4 +7,19 @@
 
 #include <iostream>
 
+using namespace std;
 
+class myRobot
+{
+public:
+	myRobot(unsigned short int X, unsigned short int Y, string dir);
+	~myRobot();
+	void place(unsigned short int new_x, unsigned short int new_y, string new_direction, myRobot newRobot);
+	void move();
+	void dontFall();
+	void report();
+private:
+	unsigned short int x;
+	unsigned short int y;
+	string direction;
+};
