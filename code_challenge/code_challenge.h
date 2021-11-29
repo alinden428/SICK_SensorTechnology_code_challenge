ee5f1ba949159d7a886aac84160d0860d6bf0cb5
Alinden <7709005@student.swin.edu.au>
2021-11-29 19:18:54 +1100

    fix merge error

--- code_challenge/code_challenge.h
+++ code_challenge/code_challenge.h
@@ -7,4 +7,21 @@
 
 #include <iostream>
 
+using namespace std;
 
+class myRobot
+{
+public:
+	myRobot();
+	~myRobot();
+	void place(unsigned short int new_x, unsigned short int new_y, string new_direction);
+	myRobot move();
+	bool dontFall();
+	void rotate(string leftOrRight);
+	void report();
+	friend istream& operator>>(istream& aInstream, myRobot& newRobot);
+private:
+	unsigned short int x;
+	unsigned short int y;
+	string direction;
+};
--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -1,2 +1,18 @@
 #include "gtest/gtest.h"
+#include "../code_challenge/code_challenge.h"
+
+bool createBot() {
+	myRobot newRobot = myRobot();
+}
+
+TEST(RobotTests, DemonstrateMacros) {
+	EXPECT_EQ(false, false);
+	EXPECT_EQ(true, false);
+}
+
+TEST(RobotTests, AnotherOne) {
+	
+	EXPECT_EQ(false, false);
+	EXPECT_EQ(true, false);
+}
 
